905b342ff5824742fca8f7a227fc85f7f064b117
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-05-10 15:24:27 -0500

    simple API request

--- src/CoffeeBar.js
+++ src/CoffeeBar.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const CoffeeBar = () => {
-  const requestCoffee = "https://coffee.alexflipnote.dev/random"
+  const [coffee, setCoffee] = useState(null);
+
+  useEffect(function fetchCoffeeWhenMounted() {
+    async function fetchCoffee(){
+      const requestCoffee = await axios.get("https://coffee.alexflipnote.dev/random");
+      setCoffee(requestCoffee.data)
+      console.log(coffee)
+    }
+    fetchCoffee();
+  }, [])
   
   return (
     <div>
-        <Coffee />
+        <img 
+          src={coffee}
+          style={{width: '700px'}}
+        />
     </div>
   )
 }
