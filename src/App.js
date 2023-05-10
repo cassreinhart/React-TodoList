7246c20b4957f4020a51091e5790aa791ca656b8
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-05-10 15:16:23 -0500

    initialize app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CoffeeBar from './CoffeeBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CoffeeBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CoffeeBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CoffeeBar = () => {
+  const requestCoffee = "https://coffee.alexflipnote.dev/random"
+  
+  return (
+    <div>
+        <Coffee />
+    </div>
+  )
+}
+
+export default CoffeeBar
